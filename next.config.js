e3e522cfc64598f7515ec99b7dd376c988cc6f2e
edcrossfire <ed@crossfire.network>
2022-07-06 15:40:18 -0400

    Updated webpack in next config

--- next.config.js
+++ next.config.js
@@ -3,5 +3,15 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
+const webpack = require('webpack')
 
-module.exports = nextConfig
+const { parsed: myEnv } = require('dotenv').config({
+  path:'C:/Users/ecart/Desktop/dev-portfolio/algo-vote-frontend/.env'
+})
+
+module.exports = {
+  webpack(config) {
+      config.plugins.push(new webpack.EnvironmentPlugin(myEnv))
+      return config
+  }
+}
