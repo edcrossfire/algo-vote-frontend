65194c3d1db0eb17621a18b2188eca70b9370c17
edcrossfire <ed@crossfire.network>
2022-07-06 15:30:05 -0400

    Removed webpack requirements for vercel

--- next.config.js
+++ next.config.js
@@ -3,15 +3,5 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-const webpack = require('webpack')
 
-const { parsed: myEnv } = require('dotenv').config({
-  path:'C:/Users/ecart/Desktop/dev-portfolio/algo-vote/.env'
-})
-
-module.exports = {
-  webpack(config) {
-      config.plugins.push(new webpack.EnvironmentPlugin(myEnv))
-      return config
-  }
-}
+module.exports = nextConfig
