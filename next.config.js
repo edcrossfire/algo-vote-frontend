768d9e15e1af96e6eeb862f8b803e2f3ad80cebb
edcrossfire <ed@crossfire.network>
2022-07-06 15:54:18 -0400

    Require dotenv

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@ const nextConfig = {
 
 const webpack = require('webpack')
 
+const { parsed: myEnv } = require('dotenv').config({
+  
+})
+
 module.exports = {
   webpack(config) {
       config.plugins.push(new webpack.EnvironmentPlugin(myEnv))
